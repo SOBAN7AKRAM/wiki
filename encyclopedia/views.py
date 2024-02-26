5cc748cfe59ebf415d3f7e87e0d34874a41b071c
USER <msobanakram7@gmail.com>
2024-02-26 19:54:31 +0500

    Implement first specification

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -7,4 +7,14 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
+def displayEntry(request, name):
+    entry = util.get_entry(name)
+    if (entry):
+        return render(request, "encyclopedia/entry.html", {
+            "entries": entry,
+            "title": name.upper()
+        })
+    else:
+        return render(request, "encyclopedia/error.html",{
+            "name" : name.upper()
+        })
