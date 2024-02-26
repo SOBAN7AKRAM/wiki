c404d427b790a980e2a830a238a26dbdb20d98b9
USER <msobanakram7@gmail.com>
2024-02-26 23:41:48 +0500

    after search specification

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:name>", views.displayEntry,name="entry")
+    path("wiki/<str:name>", views.displayEntry,name="entry"),
+    path("search/", views.search, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 from . import util
 
 
@@ -17,4 +17,22 @@ def displayEntry(request, name):
     else:
         return render(request, "encyclopedia/error.html",{
             "name" : name.upper()
+        })
+def search(request):
+    name = request.GET.get('q')
+    entry = util.get_entry(name)
+    if (entry):
+        return render(request, "encyclopedia/entry.html", {
+            "entries": entry,
+            "title": name.upper()
+        })
+    else:
+        sublist = []
+        list = util.list_entries()
+        for i in list:
+            if name.upper() in i.upper():
+                sublist.append(i)
+        return render(request, "encyclopedia/searchList.html", {
+            "entries": sublist,
+            "name": name
         })
